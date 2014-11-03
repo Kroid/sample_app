fa6e82a4a9d8eb79422e40afdb20ac2a7a232f14
George M Kroid <kroid@yandex.ru>
2014-11-03 22:51:21 +0300

    refator spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
+
 describe "Static pages" do
+
+  let(:base_title){ 'Ruby on Rails Tutorial Sample App' }
 
   describe "Home page" do
 
     it 'should have the right title' do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
 
     it "should have the content 'Sample App'" do
@@ -19,7 +22,7 @@ describe "Static pages" do
 
     it 'should have the right title' do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
 
     it "should have the content 'Help'" do
@@ -32,7 +35,7 @@ describe "Static pages" do
 
     it 'should have the right title' do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+      expect(page).to have_title("#{base_title} | About Us")
     end
 
     it "should have the content 'About Us'" do
@@ -45,7 +48,7 @@ describe "Static pages" do
 
     it 'shoud have the right title' do
       visit '/static_pages/contact'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
+      expect(page).to have_title("#{base_title} | Contact")
     end
 
     it "should have the content 'Contact'" do
