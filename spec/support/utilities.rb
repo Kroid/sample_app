7fc72e36f8baabee30f11898995f44f657575551
George M Kroid <kroid@yandex.ru>
2014-11-04 17:21:35 +0300

    finish ch5

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,1 @@
-module Utilities
-
-  def full_title(page_title = '')
-    title = 'Ruby on Rails Tutorial Sample App'
-    title = "#{title} | #{page_title}" unless page_title.empty?
-    title
-  end
-end
+include ApplicationHelper
